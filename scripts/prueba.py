b869b8d2dc50e10bd3df707b589e1c3ebfc3822f
Adrian Llanes <ados.adrian@gmail.com>
2024-08-03 22:27:28 -0300

    agregado jobstore con postgres y loggeando en la salida de webui

--- scripts/prueba.py
+++ scripts/prueba.py
@@ -1,11 +1,14 @@
 import os
+from src.log import server_log
 import requests
+# from scripts.utils_logging import logger
 
 def funcion_prueba():
     print('Ejecutando funcion de prubea')
+    server_log.info('Ejecutando funcion de prubea')
 
 def iniciar_sincronizacion_catalogo():
-    print('Ejecutando sincronizacion de catalogo vtex...')
+    server_log.info('Ejecutando sincronizacion de catalogo vtex...')
     domain = 'localhost'
     port = '9000'
     base_url = f'http://{domain}:{port}/api'
@@ -21,18 +24,19 @@ def iniciar_sincronizacion_catalogo():
         'verificar_links': False,
         'completo': False
     }
+    server_log.info(f'    Ejecutando consulta...')
     response = requests.get(
         url=url,
         params=params,
         headers=headers
     )
     if response.status_code == 200:
-        print("Successfully triggered catalogo_vtex sync")
-        # logger.info("Successfully triggered catalogo_vtex sync")
+        # print("Successfully triggered catalogo_vtex sync")
+        server_log.info("Successfully triggered catalogo_vtex sync")
         # last_sync_status = {"timestamp": datetime.now(), "status": "Success"}
     else:
-        print(f"Failed to trigger catalogo_vtex sync. Status code: {response.status_code}")
-        # logger.error(f"Failed to trigger catalogo_vtex sync. Status code: {response.status_code}")
+        # print(f"Failed to trigger catalogo_vtex sync. Status code: {response.status_code}")
+        server_log.error(f"Failed to trigger catalogo_vtex sync. Status code: {response.status_code}")
         # last_sync_status = {"timestamp": datetime.now(), "status": f"Failed - Status code: {response.status_code}"}
 
 if __name__ == '__main__':
--- src/config.py
+++ src/config.py
@@ -1,11 +1,14 @@
 from pathlib import Path
 
 from apscheduler.executors.asyncio import AsyncIOExecutor
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 
 ROOT = Path(__file__).parent.parent
 LOG_PATH = ROOT / "logs"
 
 SCHEDULER_CONFIG = {
     "executors": {"default": AsyncIOExecutor()},
-    "jobstores": {},
+    "jobstores": {"postgres": SQLAlchemyJobStore(
+        url='postgresql://adrian:1223@localhost:5432/scheduler-db'
+    )},
 }
